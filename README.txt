d8430be8d05d5070a0c79f52a8128969fe1e4e6d
gbiobob <gbiobob+git@gmail.com>
2017-01-26 09:58:27 +0100

    Add files

---
+++ README.txt
@@ -0,0 +1,21 @@
+# ED-Board - Overwolf app
+
+An Elite: Dangerous app for Overwolf overlay (embed the en.ed-board.net app).
+
+ED-Board is a log book & more for commanders who want to keep informations about their interstellar trips.
+(an account on the web application en.ed-board.net is needed, registration can be made directly from the Overwolf app)
+
+## Main features
+- Log book : enter informations for yourself on everything (like a notepad) & since recently register your current trades routes (find by yourself or on a third party tools),
+- Cmdr page : register your position, ranks, levels and credit benefits,
+- Dock : register your owned ship and track you progress to buy a more expensive ship (based on your benefits),
+- Groups : create a private group (name, small desc & url of your group / guild / community, ...), invite members, share your position & make schedules between member to organize cooperative flights (Wings),
+- choose the application color theme to fit with your HUD color,
+- get an in-game wiki,
+- listen E:D related radios.
+
+## Languages available
+* English
+* French
+* Russian
+* Spanish
---
+++ app/Files/edboard.js
@@ -0,0 +1,313 @@
+/**
+ * Main vars
+ */
+
+var baseurlEdB = 'en.ed-board.net';
+
+/**
+ * Page launch
+ */
+
+$(function() {
+
+  start();
+  initButtons();
+
+});
+
+$('body').css('zoom','200%');
+
+/**
+ * Buttons actions
+ */
+
+ function initButtons() {
+
+  //-- Lang change
+  $('.lang').click(function() {
+   lang = $(this).attr('data-lang');
+   localStorage.setItem('lang',lang);
+    reloadFavNote();
+    $('#lang_choose').hide();
+    $('#content').show();
+  });
+
+  //-- Open/close fav list
+  $('#action a.toggle').click(function() {
+    $('#action a.toggle').toggle();
+    $('#infos iframe').toggle();
+  });
+
+  //-- Refresh notes fav list
+  $('#action a#reload').click(function() {
+    reloadFavNote();
+  });
+
+  $('#open-options').click(function() {
+    $('#lang_choose').show();
+    $('#content').hide();
+  });
+
+  $('.orientation').click(function() {
+    $('.orientation').toggle();
+    $('#content').toggleClass('vertical');
+  });
+
+  var show = 0;
+  $('.auto_hide').click(function() {
+    show = (show+1)%3;
+    $('.auto_hide').hide();
+    $('.auto_hide.hide-'+show).show();
+    $('#content').removeClass('hide-0 hide-1 hide-2');
+    $('#content').addClass('hide-'+show);
+  });
+
+}
+
+/**
+ * Reload fav notes iframe
+ */
+
+function reloadFavNote() {
+  loadDatas();
+}
+
+/**
+ * Start load : Lang chooser
+ */
+
+function start() {
+
+  var lang = localStorage.getItem('lang');
+
+  if(lang==null) {
+    $('#lang_choose').show();
+    $('#content').hide();
+  } else {
+    $('#lang_choose').hide();
+    $('#content').show();
+  }
+
+
+  $('.color-opt').each(function(){
+    var color = $(this).attr('data-color');
+    $(this).css('background-color','#'+color);
+  });
+
+  $('.color-opt').click(function() {
+    var color = $(this).attr('data-color');
+    $('#color-pick').val(color);
+    $('#color-pick').keyup();
+  });
+
+
+  loadDatas();
+  changeColor();
+
+}
+
+/**
+ * Load user data from ed-board.net
+ */
+
+function loadDatas() {
+
+  var lang = localStorage.getItem('lang');
+  var color = localStorage.getItem('opt_color');
+
+  baseurlEdB = getUrlLang();
+
+  var params = $('#datas').attr('data-url');
+  if (color!='' && color!='undefined' && color!='null') {
+    params += '&color='+color;
+    colorUI(color);
+  }
+  $('#datas').attr('src', 'http://'+baseurlEdB+params+'&utm_source=overwolf&utm_medium=app&utm_campaign=ovedboard');
+}
+
+/**
+ * Update UI color
+ */
+
+function changeColor() {
+  var color = localStorage.getItem('opt_color');
+  if(color==null) {
+    color = 'FD610F';
+  } else {
+    color = rgb2hex(color);
+  }
+
+  $('#color-pick').val(color);
+
+  $('#color-pick').keyup(function(){
+    var color = $(this).val();
+    if(color.length == 6) {
+      localStorage.setItem('opt_color',hexToRgb(color));
+      colorUI(hexToRgb(color));
+      loadDatas();
+    }
+  });
+}
+
+/**
+ * Update UI color
+ */
+
+function colorUI(color) {
+  $('body *').css({"color":"rgb("+color+")"});
+  $('body *').css({"border-color":"rgb("+color+")"});
+}
+
+/**
+ * Get iframe lang url
+ */
+
+function getUrlLang() {
+
+  var lang = localStorage.getItem('lang');
+
+  switch(lang) {
+    case 'fr':
+      return 'ed-board.net';
+      break;
+
+    case 'ru':
+      return 'ru.ed-board.net';
+      break;
+
+    case 'es':
+      return 'es.ed-board.net';
+      break;
+
+    case 'en':
+    default:
+      return 'en.ed-board.net';
+      break;
+  }
+}
+
+/**
+ * Color Hex to RGB
+ */
+
+function hexToRgb(hex) {
+hex = parseInt(hex,16);
+    var r = hex >> 16;
+    var g = hex >> 8 & 0xFF;
+    var b = hex & 0xFF;
+    return r+','+g+','+b;
+}
+
+function rgb2hex(rgb) {
+ rgb = rgb.match(/^[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+ return (rgb && rgb.length === 4) ?
+  ("0" + parseInt(rgb[0],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) : '';
+}
+
+/**
+ * Radio
+ */
+
+var withRadio = false;
+function openSubRadio(){
+  overwolf.windows.obtainDeclaredWindow("SubRadio", function(result){
+    if (result.status == "success"){
+      overwolf.windows.restore(result.window.id, function(result){
+          console.log(result);
+      });
+    }
+  });
+};
+
+/**
+ * Overwolf functions
+ */
+
+function dragResize(edge){
+  overwolf.windows.getCurrentWindow(function(result){
+    if (result.status=="success"){
+      overwolf.windows.dragResize(result.window.id, edge);
+    }
+  });
+};
+
+function dragMove() {
+  overwolf.windows.getCurrentWindow(function(result){
+    if (result.status=="success"){
+      overwolf.windows.dragMove(result.window.id);
+    }
+  });
+};
+
+function closeWindow(){
+  overwolf.windows.getCurrentWindow(function(result){
+    if (result.status=="success"){
+      overwolf.windows.close(result.window.id);
+    }
+  });
+};
+
+function minimizeWindow(){
+  overwolf.windows.getCurrentWindow(function(result){
+    if (result.status=="success"){
+      overwolf.windows.minimize(result.window.id);
+    }
+  });
+};
+
+function openSubNotes(){
+  overwolf.windows.obtainDeclaredWindow("SubNotes", function(result){
+    if (result.status == "success"){
+      overwolf.windows.restore(result.window.id, function(result){
+          console.log(result);
+      });
+    }
+  });
+};
+
+function openSubCmdr(){
+  overwolf.windows.obtainDeclaredWindow("SubCmdr", function(result){
+    if (result.status == "success"){
+      overwolf.windows.restore(result.window.id, function(result){
+          console.log(result);
+      });
+    }
+  });
+};
+
+function openSubHangar(){
+  overwolf.windows.obtainDeclaredWindow("SubHangar", function(result){
+    if (result.status == "success"){
+      overwolf.windows.restore(result.window.id, function(result){
+          console.log(result);
+      });
+    }
+  });
+};
+
+function openSubGroup(){
+  overwolf.windows.obtainDeclaredWindow("SubGroup", function(result){
+    if (result.status == "success"){
+      overwolf.windows.restore(result.window.id, function(result){
+          console.log(result);
+      });
+    }
+  });
+};
+
+function openSubWiki(){
+  overwolf.windows.obtainDeclaredWindow("SubWiki", function(result){
+    if (result.status == "success"){
+      overwolf.windows.restore(result.window.id, function(result){
+          console.log(result);
+      });
+    }
+  });
+};
+
+
+
+
+
